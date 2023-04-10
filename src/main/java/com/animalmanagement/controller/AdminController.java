160251c18a52c064a71175c36243b3f94ebdc997
郝鹏程 <20373487@buaa.edu.cn>
2023-04-10 18:01:50 +0800

    change

--- src/main/java/com/animalmanagement/controller/AdminController.java
+++ src/main/java/com/animalmanagement/controller/AdminController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/admin")
 public class AdminController {
 
 }
