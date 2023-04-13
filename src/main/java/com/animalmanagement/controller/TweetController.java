a613baad7180e98e3d3defd98a27c9c81ef4dccf
MaYouSYQ <mayousyq@gmail.com>
2023-04-13 08:31:05 +0800

    添加了TweetController

---
+++ src/main/java/com/animalmanagement/controller/TweetController.java
@@ -0,0 +1,22 @@
+package com.animalmanagement.controller;
+
+import com.animalmanagement.bean.BaseResponse;
+import com.animalmanagement.bean.bo.*;
+import com.animalmanagement.enums.StatusEnum;
+import com.animalmanagement.service.AccountService;
+import com.animalmanagement.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/tweet")
+public class TweetController {
+
+    @Autowired
+    AccountService accountService;
+
+    @Autowired
+    UserService userService;
+
+    
+}
