38f2877d7837a1bc07d650503b5c99856cf7a212
MaYouSYQ <mayousyq@gmail.com>
2023-04-25 03:08:49 +0800

    添加了领养表，实现了了领养接口

---
+++ src/main/java/com/animalmanagement/bean/vo/AdminAdoptionGetVo.java
@@ -0,0 +1,16 @@
+package com.animalmanagement.bean.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class AdminAdoptionGetVo {
+    private Integer id;
+    private String username;
+    private String aniname;
+}
