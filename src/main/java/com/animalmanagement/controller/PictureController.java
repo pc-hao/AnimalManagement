7d0e85be8226d0a363eeff2876074d8e8c4efecd
郝鹏程 <20373487@buaa.edu.cn>
2023-04-24 09:49:56 +0800

    完成图片上传接口

---
+++ src/main/java/com/animalmanagement/controller/PictureController.java
@@ -0,0 +1,51 @@
+package com.animalmanagement.controller;
+
+import com.animalmanagement.bean.BaseResponse;
+import com.animalmanagement.enums.StatusEnum;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+@RestController
+@RequestMapping("/picture")
+public class PictureController {
+    private static final String ProfilePhotoSavePath = "/root/AnimalManagement/images";
+
+    private static final List<String> suffixNameList = Arrays.asList("jpg", "jpeg", "png");
+
+    @PostMapping("/upload")
+    public BaseResponse profilePhotoUpload(@RequestParam("image") MultipartFile fileUpload, @RequestParam("type") String fileType) throws IOException {
+        //获取文件名
+        String fileName = fileUpload.getOriginalFilename();
+        //获取文件后缀名
+        String suffixName = fileName.substring(fileName.lastIndexOf("."));
+        if (!suffixNameList.stream().anyMatch(e -> e.equals(suffixName))) {
+            throw new RuntimeException("Image Format Is Illegal!");
+        }
+        //为了避免发生图片替换，这里使用了文件名重新生成
+        fileName = UUID.randomUUID() + suffixName;
+        String filePath = null;
+        if ("animal".equals(fileType)) {
+            filePath = ProfilePhotoSavePath + "/animal" + fileName;
+        } else if ("tweet".equals(fileType) || "help".equals(fileType)) {
+            filePath = ProfilePhotoSavePath + "/tweet" + fileName;
+        } else if ("user".equals(fileType)) {
+            filePath = ProfilePhotoSavePath + "/user" + fileName;
+        }
+        if (Objects.isNull(filePath)) {
+            throw new RuntimeException("Image Type Is Illegal!");
+        }
+        //将图片保存到文件夹里
+        fileUpload.transferTo(new File(filePath));
+
+        HashMap<String, String> result = new HashMap<>();
+        result.put("imagePath", filePath);
+        return BaseResponse.builder().code(StatusEnum.SUCCESS.getCode()).body(result).build();
+    }
+}
