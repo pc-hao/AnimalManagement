7a41dea86bc34ac4fd477e07abdbb3f5456e9dda
MaYouSYQ <mayousyq@gmail.com>
2023-04-25 01:42:03 +0800

    完成了用户获取求助的接口

---
+++ src/main/java/com/animalmanagement/bean/bo/UserHelpGetBo.java
@@ -0,0 +1,14 @@
+package com.animalmanagement.bean.bo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserHelpGetBo {
+    private Integer pageNum;
+    private Integer page;
+    private String context;
+}
