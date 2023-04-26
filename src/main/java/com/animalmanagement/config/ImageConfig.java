83341cc5435ad59e212fe5ddd36a4f695b92055c
MaYouSYQ <mayousyq@gmail.com>
2023-04-26 21:37:06 +0800

    修改了imageconfig

--- src/main/java/com/animalmanagement/config/ImageConfig.java
+++ src/main/java/com/animalmanagement/config/ImageConfig.java
@@ -6,10 +6,16 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Getter
-@Setter
 @Component
 @ConfigurationProperties(prefix = "image")
 public class ImageConfig {
     public static String savePath;
     public static String frontPath;
+
+    public void setSavePath(String savePath) {
+        this.savePath = savePath;
+    }
+    public void setFrontPath(String frontPath) {
+        this.frontPath = frontPath;
+    }
 }
