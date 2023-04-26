9174596fc0f43fcd5e1f5ab220570e6c890e4651
MaYouSYQ <mayousyq@gmail.com>
2023-04-26 21:42:59 +0800

    彻底修好了用户图片接口

--- src/main/java/com/animalmanagement/service/UserService.java
+++ src/main/java/com/animalmanagement/service/UserService.java
@@ -30,7 +30,9 @@ import com.animalmanagement.utils.EncodeUtil;
 
 @Service
 public class UserService {
-    private static final String PICTURE_SAVE_PATH = ImageConfig.frontPath + "/user/";
+    private static final String PICTURE_SAVE_PATH_FRONT = ImageConfig.frontPath + "/user/";
+
+    private static final String PICTURE_SAVE_PATH = ImageConfig.savePath + "/user/";
 
     private static final String DEFAULT_IMAGE_PATH = ImageConfig.frontPath + "/user/default.png";
 
@@ -143,12 +145,13 @@ public class UserService {
         }
         if (modifyUserInfoBo.getAvatar() != null && !modifyUserInfoBo.getAvatar().isEmpty()) {
             String newAvatar = PICTURE_SAVE_PATH + userInfo.getId() + ".png";
+            String newAvatarFront = PICTURE_SAVE_PATH_FRONT + userInfo.getId() + ".png";
             try {
                 Files.move(Paths.get(modifyUserInfoBo.getAvatar()), Paths.get(newAvatar), StandardCopyOption.REPLACE_EXISTING);
             } catch (IOException e) {
                 throw new RuntimeException(e.getMessage());
             }
-            userInfo.setAvatar(newAvatar);
+            userInfo.setAvatar(newAvatarFront);
         }
 
         sysUserMapper.updateByPrimaryKeySelective(sysUser);
