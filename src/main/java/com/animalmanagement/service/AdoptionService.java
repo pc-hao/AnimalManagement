5a8e6339eb795fd1c9b72785ca4180f267d2a06a
MaYouSYQ <mayousyq@gmail.com>
2023-05-13 15:32:24 +0800

    修改了AdminAdoptionCensor接口使其修改动物的领养状态

--- src/main/java/com/animalmanagement/service/AdoptionService.java
+++ src/main/java/com/animalmanagement/service/AdoptionService.java
@@ -108,6 +108,8 @@ public class AdoptionService {
 
         if(adminAdoptionCensorBo.getOperate() == 0) {
             adoption.setCensored(CensorStatusEnum.PASS.getCode());
+            Animal animal = animalMapper.selectByPrimaryKey(adoption.getAnimalId());
+            animal.setAdopted(true);
             message = Message.builder()
             .userId(adoption.getUserId())
             .content("您的领养申请已通过")
