1960083f45e0964f8980ba88f76c677b978cebb6
郝鹏程 <20373487@buaa.edu.cn>
2023-05-26 13:37:36 +0800

    搜索为空的时候不存入记录表里

--- src/main/java/com/animalmanagement/service/SearchLogService.java
+++ src/main/java/com/animalmanagement/service/SearchLogService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -18,8 +19,11 @@ public class SearchLogService {
     @Autowired
     SearchLogMapper searchLogMapper;
 
-    public void insertSearchLog(String context, boolean isHelp){
-        if(context.length() > 512) {
+    public void insertSearchLog(String context, boolean isHelp) {
+        if (Objects.isNull(context) || context.isEmpty()) {
+            return;
+        }
+        if (context.length() > 512) {
             throw new RuntimeException("Search Word Is Too Long!");
         }
         SearchLog searchLog = SearchLog.builder()
@@ -37,11 +41,11 @@ public class SearchLogService {
                 .map(SearchLog::getContext)
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
                 .entrySet()
-                    .stream()
-                    .sorted((o1,o2)-> o2.getValue().compareTo(o1.getValue()))
-                    .limit(num)
-                    .map(Map.Entry::getKey)
-                    .collect(Collectors.toList());
+                .stream()
+                .sorted((o1, o2) -> o2.getValue().compareTo(o1.getValue()))
+                .limit(num)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
     }
 
     public List<String> getSelf(boolean isHelp) {
