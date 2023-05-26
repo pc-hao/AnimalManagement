f32279613d80df4a085ca2c51e2376b811150377
郝鹏程 <20373487@buaa.edu.cn>
2023-05-26 11:40:22 +0800

    添加上传文件大小限制配置

--- src/main/java/com/animalmanagement/AnimalManagementApplication.java
+++ src/main/java/com/animalmanagement/AnimalManagementApplication.java
@@ -3,10 +3,24 @@ package com.animalmanagement;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.util.unit.DataSize;
+
+import javax.servlet.MultipartConfigElement;
 
 @SpringBootApplication
 @MapperScan(basePackages = {"com.animalmanagement.mapper"})
 public class AnimalManagementApplication {
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        // 文件最大
+        factory.setMaxFileSize(DataSize.parse("10MB"));
+        // 设置总上传数据总大小
+        factory.setMaxRequestSize(DataSize.parse("100MB"));
+        return factory.createMultipartConfig();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AnimalManagementApplication.class, args);
